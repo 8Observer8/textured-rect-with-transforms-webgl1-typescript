83cd69e8d7e46ba4ce97c8d27b88c39c3ed6e2a2
Ivan Enzhaev <8observer8@gmail.com>
2019-10-19 14:42:20 +0400

    Add Playgrounds

--- src/client/Program.ts
+++ src/client/Program.ts
@@ -1,5 +1,9 @@
 import MyScene from "./MyScene";
 
+// Playgrounds:
+// Plunker: https://next.plnkr.co/edit/4pnm93F1eWQuvpYg?preview
+// CodeSandbox: https://codesandbox.io/s/textured-rectangle-with-transforms-typescript-s7gfb
+
 class Program
 {
     public static Main(): void
